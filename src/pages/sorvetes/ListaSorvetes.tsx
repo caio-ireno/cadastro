562814f7d9c9eb15ca32c0a97eb35130e23d399b
caio-ireno <caio_ireno@hotmail.com>
2023-01-31 13:13:56 -0300

    debounce  mock

--- src/pages/sorvetes/ListaSorvetes.tsx
+++ src/pages/sorvetes/ListaSorvetes.tsx
@@ -3,24 +3,27 @@ import React, { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { FerramentasDaLista } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { SorveteService } from '../../shared/services/api/sorvete/SorveteService';
 
 export const ListaSorvetes: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    SorveteService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-        return;
-      }
-
-      console.log(result);
+    debounce(() => {
+      SorveteService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+          return;
+        }
+        console.log(result);
+      });
     });
   }, [busca]);
 
