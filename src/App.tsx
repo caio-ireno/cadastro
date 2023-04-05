d132df4844d03fbd4049f9a3c4bd34ac6c3e46f5
caio-ireno <caio_ireno@hotmail.com>
2023-04-05 12:25:33 -0300

    rotas praticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { DetalheContatoAdm } from './pages/adm/DetalheContatoAdm'
-import { DetalheHistoriaAdm } from './pages/adm/DetalheHistoriaAdm'
-import { DetalheLojasAdm } from './pages/adm/DetalheLojasAdm'
-import { DetalheNoticiasAdm } from './pages/adm/DetalheNoticiasAdm'
-import { DetalheSorveteAdm } from './pages/adm/DetalheSorveteAdm'
-import { DetalheTipoSorvete } from './pages/adm/DetalheTipoSorvete'
-import { LojaAdm } from './pages/adm/LojaAdm'
-import { NoticiaAdm } from './pages/adm/NoticiaAdm'
-import { SorveteAdm } from './pages/adm/SorveteAdm'
-import { TipoSorveteAdms } from './pages/adm/TipoSorveteAdm'
 import { Login } from './pages/login/Login'
 import { AppRoutes } from './routes'
+import { AdmRoute } from './routes/AdmRoute'
 import { MenuLateral } from './shared/components'
 import { AppThemeProvider, DrawerProvider } from './shared/contexts'
 import ScrollToTopOnNavigate from './shared/hooks/ScrollToTopNavigate'
@@ -24,40 +15,7 @@ export const App = () => {
           <ScrollToTopOnNavigate />
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/adm-page" element={<SorveteAdm />} />
-            <Route path="/adm-page/sorvetes" element={<SorveteAdm />} />
-            <Route
-              path="/adm-page/sorvetes/:id"
-              element={<DetalheSorveteAdm />}
-            />
-
-            <Route path="/adm-page/noticias" element={<NoticiaAdm />} />
-            <Route
-              path="/adm-page/noticias/:id"
-              element={<DetalheNoticiasAdm />}
-            />
-
-            <Route path="/adm-page/lojas" element={<LojaAdm />} />
-            <Route path="/adm-page/lojas/:id" element={<DetalheLojasAdm />} />
-
-            <Route
-              path="/adm-page/historias/:id"
-              element={<DetalheHistoriaAdm />}
-            />
-
-            <Route
-              path="/adm-page/contato/:id"
-              element={<DetalheContatoAdm />}
-            />
-
-            <Route
-              path="/adm-page/tipo-sorvete"
-              element={<TipoSorveteAdms />}
-            />
-            <Route
-              path="/adm-page/tipo-sorvete/:id"
-              element={<DetalheTipoSorvete />}
-            />
+            <Route path="/adm-page/*" element={<AdmRoute />} />
             <Route
               path="*"
               element={
---
+++ src/routes/AdmRoute.tsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { DetalheContatoAdm } from '../pages/adm/DetalheContatoAdm'
+import { DetalheHistoriaAdm } from '../pages/adm/DetalheHistoriaAdm'
+import { DetalheLojasAdm } from '../pages/adm/DetalheLojasAdm'
+import { DetalheNoticiasAdm } from '../pages/adm/DetalheNoticiasAdm'
+import { DetalheSorveteAdm } from '../pages/adm/DetalheSorveteAdm'
+import { DetalheTipoSorvete } from '../pages/adm/DetalheTipoSorvete'
+import { LojaAdm } from '../pages/adm/LojaAdm'
+import { NoticiaAdm } from '../pages/adm/NoticiaAdm'
+import { SorveteAdm } from '../pages/adm/SorveteAdm'
+import { TipoSorveteAdms } from '../pages/adm/TipoSorveteAdm'
+
+export const AdmRoute = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SorveteAdm />} />
+      <Route path="/sorvetes" element={<SorveteAdm />} />
+      <Route path="/sorvetes/:id" element={<DetalheSorveteAdm />} />
+
+      <Route path="/noticias" element={<NoticiaAdm />} />
+      <Route path="/noticias/:id" element={<DetalheNoticiasAdm />} />
+
+      <Route path="/lojas" element={<LojaAdm />} />
+      <Route path="/lojas/:id" element={<DetalheLojasAdm />} />
+
+      <Route path="/historias/:id" element={<DetalheHistoriaAdm />} />
+
+      <Route path="/contato/:id" element={<DetalheContatoAdm />} />
+
+      <Route path="/tipo-sorvete" element={<TipoSorveteAdms />} />
+      <Route path="/tipo-sorvete/:id" element={<DetalheTipoSorvete />} />
+    </Routes>
+  )
+}
