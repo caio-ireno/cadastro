33e39dd82cb58c6f69293b4a142b63103c7a7284
caio-ireno <caio_ireno@hotmail.com>
2023-04-05 11:32:06 -0300

    without menulateral props

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,6 @@ import { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { Dashboard, ListaSorvetes } from '../pages'
-import { DetalheContatoAdm } from '../pages/adm/DetalheContatoAdm'
-import { DetalheHistoriaAdm } from '../pages/adm/DetalheHistoriaAdm'
-import { DetalheLojasAdm } from '../pages/adm/DetalheLojasAdm'
-import { DetalheNoticiasAdm } from '../pages/adm/DetalheNoticiasAdm'
-import { DetalheSorveteAdm } from '../pages/adm/DetalheSorveteAdm'
-import { DetalheTipoSorvete } from '../pages/adm/DetalheTipoSorvete'
-import { LojaAdm } from '../pages/adm/LojaAdm'
-import { NoticiaAdm } from '../pages/adm/NoticiaAdm'
-import { SorveteAdm } from '../pages/adm/SorveteAdm'
-import { TipoSorveteAdms } from '../pages/adm/TipoSorveteAdm'
 import { Historia } from '../pages/historia/Historia'
 import { ListaLojas } from '../pages/lojas/ListaLojas'
 import { useDrawerContext } from '../shared/contexts'
@@ -46,26 +36,6 @@ export const AppRoutes = () => {
       <Route path="/pagina-inicial" element={<Dashboard />} />
       <Route path="/lojas" element={<ListaLojas />} />
       <Route path="/historia" element={<Historia />} />
-
-      <Route path="/adm-page" element={<SorveteAdm />} />
-      <Route path="/adm-page/sorvetes" element={<SorveteAdm />} />
-      <Route path="/adm-page/sorvetes/:id" element={<DetalheSorveteAdm />} />
-
-      <Route path="/adm-page/noticias" element={<NoticiaAdm />} />
-      <Route path="/adm-page/noticias/:id" element={<DetalheNoticiasAdm />} />
-
-      <Route path="/adm-page/lojas" element={<LojaAdm />} />
-      <Route path="/adm-page/lojas/:id" element={<DetalheLojasAdm />} />
-
-      <Route path="/adm-page/historias/:id" element={<DetalheHistoriaAdm />} />
-
-      <Route path="/adm-page/contato/:id" element={<DetalheContatoAdm />} />
-
-      <Route path="/adm-page/tipo-sorvete" element={<TipoSorveteAdms />} />
-      <Route
-        path="/adm-page/tipo-sorvete/:id"
-        element={<DetalheTipoSorvete />}
-      />
 
       <Route path="/sorvetes" element={<ListaSorvetes />} />
 
